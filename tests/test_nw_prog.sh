45f1d32e2099a16bd695d0c9dfb66e2273d8baed
Thomas Junier <thomas.junier@unige.ch>
2008-01-15 09:54:04 +0000

    More app tests

--- tests/test_nw_prog.sh
+++ tests/test_nw_prog.sh
@@ -3,19 +3,33 @@
 
 prog=${0%.sh}	# derive tested program's name from own name
 prog=${prog#*_}
+args_file=test_${prog}_args
 
 echo "Testing program: $prog"
+
+if [ !  -x ../src/$prog ] ; then
+	echo "$prog not found or not executable."
+	exit 1
+fi
+
+if [ ! -r $args_file ] ; then
+	echo "can't find arguments file $args_file"
+	exit 1
+fi
+
 pass=TRUE
 while IFS=':' read name args ; do
-	echo -n "test: $name - "
-	../src/$prog $args > test_${prog}_$name.out
+	# setting IFS to '' preserves whitespace through shell word splitting
+	IFS='' cmd="../src/$prog $args"
+	echo -n "test: '$cmd' - "
+	eval $cmd > test_${prog}_$name.out
 	if diff test_${prog}_$name.out test_${prog}_$name.exp ; then
 		echo "pass"
 	else
 		echo "FAIL"
 		pass=FALSE
 	fi
-done < test_${prog}_args
+done < $args_file
 
 if test $pass = FALSE ; then
 	exit 1
