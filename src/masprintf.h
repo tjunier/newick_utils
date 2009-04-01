6d837737e6b2bf75e7c15eae17bb3d12d893f7a9
Thomas Junier <thomas.junier@unige.ch>
2009-04-01 21:08:06 +0000

    All programs have a multiple-tree test case

--- src/masprintf.h
+++ src/masprintf.h
@@ -4,5 +4,6 @@
 char *masprintf(const char *format, ...);
 
 /* like strcat(3), but reallocates storage if needed (which must be freed). */
+/* Not sire we'll keep this one - I wrote it when I wanted to put all multiple SVG trees in a single file, which is no longer how things are being done. Still, it might come in handy. */
 
 char *mastrcat(const char *src, const char *dest);
