e994ad7ce158e97b020e208dd5fa3ed3c5e57d12
Thomas Junier <thomas.junier@unige.ch>
2009-07-15 09:03:03 +0000

    bootscan.sh now does only classic bootscan

--- src/bootscan.sh
+++ src/bootscan.sh
@@ -6,25 +6,18 @@
 # cooperation with other command-line bioinformatics tools.
 
 # The purpose of the program is to find recombination breakpoints in a sequence
-# (hereafter called the 'reference') by compring it to related sequences over
+# (hereafter called the 'reference') by comparing it to related sequences over
 # an alignment. If the reference's nearest neighbor changes, then there is
 # evidence of recombination.
 
 # The program takes a multiple sequence file (FastA, unaligned), the ID of the
 # outgroup, and the ID of the reference. Then i) it aligns the sequences; ii)
 # it slices the alignments into windows ('slices'); iii) it computes a tree for
-# each window. Then it makes two plots:
-
-# In the first plot, the distance (along the tree branches) between the
-# reference and all other sequences is plotted against alignment position. This
-# is similar to the classical bootscan, which plotted percent identity. If the
-# sequence with smallest distance changes drastically, you may have a
-# breakpoint.
-
-# The second plot shows which sequences are in a neighborhood of the reference,
-# where neighborhood is defined as a distance threshold (currently, 40% of the
-# difference between the most distant and closest sequences). If the set of
-# neighbors changes drastically, you may have a breakpoint.
+# each window. Then it makes a plot. In the plot, the distance (along the tree
+# branches) between the reference and all other sequences is plotted against
+# alignment position. This is similar to the classical bootscan, which plotted
+# percent identity. If the sequence with smallest distance changes drastically,
+# you may have a breakpoint.
 
 # Requires Muscle [1], EMBOSS [2], and PhyML [3], GNUPlot [5]; as
 # well as the GNU core utilities [4] (which you probably already have if you're
@@ -78,16 +71,15 @@ reroot_trees()
 }
 
 # This extract the following data
-# - a list of all labels, taken from the most frequent topology (this has some
-# influence on the layout of the neighborhood bootscan)
+# - a list of all labels
 # - the number of labels
 # - the index of the reference in the list of labels
 # - the list of all labels _except_ the reference
 
 label_data()
 {
-	modal_topology=$(cat ${MUSCLE_OUT}_slice*.rr.nw | nw_topology -I - | nw_order - | sort | uniq -c | sort -nr | head -1 | sed 's/[0-9]\+//')
-	labels=($(echo $modal_topology | nw_clade -s - $OUTGROUP | nw_labels -I -))
+	first_tree=$(ls ${MUSCLE_OUT}_slice*.rr.nw | head -1)
+	labels=($(nw_clade -s $first_tree $OUTGROUP | nw_labels -I -))
 	nb_labels=${#labels[*]}
 	# This gives the index of the reference in the list of labels (starting at 1)
 	ref_ndx=$(echo ${labels[*]} | tr ' ' "\n" | awk -vref=$REFERENCE '$1 == ref {print NR}')
@@ -128,56 +120,10 @@ plot_classic()
 	gnuplot $DIST_GNUPLOT
 }
 
-# This function is similar to extract_distances_noref(), but it does NOT remove
-# the reference (so we do not need to correct for the missing reference when we
-# loop over all columns).
-
-extract_distances()
+cleanup()
 {
-	for rooted_tree in ${MUSCLE_OUT}_slice_*.rr.nw ; do 
-		position=${rooted_tree/*_slice_/}
-		position=${position/-*}
-		echo -n "$position	"	# TAB!
-		# We compute a matrix of all-vs-all distances, but only keep the line for
-		# reference vs. all
-		nw_distance -mm -n $rooted_tree ${labels[*]} | sed -n '2,$p' | grep $REFERENCE
-	done | cut -f1,3- | sort -k1n > $DIST_WREF
-}
-
-extract_neighborhoods()
-{
-	for i in $(seq $nb_labels); do
-		# use i-1 for the index of a label in the $labels array (start at 0)
-		# use i+1 for the corresponding column in the distances file (start at 2)
-		[[ $ref_ndx = $i ]] && continue	# Skip reference
-		printf "# column %d - %s\n" $((i+1)) ${labels[$((i-1))]}
-		awk -vsz=$SLICE_WIDTH \
-			-vt=$R_DISTANCE_THRESHOLD \
-			-vcol=$((i+1)) \
-			'{min=$2; max=$2; for (i=3; i<=NF; i++) { if ($i > max) max = $i; if ($i < min) min = $i; }; if (($col-min)/(max-min) <= t) { print $1+sz/2, col-1;}}' < $DIST_WREF
-		printf "\n"
-	done > $NEIGHBORHOODS
-}
-
-plot_neighborhood()
-{
-	printf "set terminal png size 700, 450\n" > $NHBD_GNUPLOT
-	printf "set output '%s'\n" $NHBD_IMAGE >> $NHBD_GNUPLOT
-	printf "set title 'Neighborhood Bootscanning of %s WRT %s, slice size %d nt'\n" \
-		$INPUT_FILE $REFERENCE $SLICE_WIDTH >> $NHBD_GNUPLOT
-	printf 'set ytics ("%s" %d' ${labels[0]} 1 >> $NHBD_GNUPLOT
-	# Sets the y-axis tics (labels)
-	for i in $(seq 2 $nb_labels) ; do
-		# use i-1 for the index of a label in the $labels array (start at 0)
-		# use i+1 for the corresponding column in the distances file (start at 2)
-		printf ', "%s" %d' ${labels[$((i-1))]} $i >> $NHBD_GNUPLOT
-	done
-	printf ")\n" >> $NHBD_GNUPLOT
-	printf "set xlabel 'position of slice centre in alignment [nt]'\n" >> $NHBD_GNUPLOT
-	printf "set ylabel 'neighbors at less than %g relative distance'\n" $R_DISTANCE_THRESHOLD >> $NHBD_GNUPLOT
-	printf "plot [][0:%d] '%s', %d title \"reference\"" $((nb_labels+1)) $NEIGHBORHOODS $ref_ndx >> $NHBD_GNUPLOT
-
-	gnuplot $NHBD_GNUPLOT
+	rm $MUSCLE_OUT $DIST_NOREF $DIST_GNUPLOT
+	rm ${MUSCLE_OUT}_slice_*.phy*
 }
 
 ################################################################
@@ -193,17 +139,13 @@ declare -r OUTGROUP=$2
 declare -r REFERENCE=$3
 declare -ri SLICE_WIDTH=300	# residues
 declare -ri SLICE_STEP=50	# slice every SLICE_STEP residues
-declare -ri BOOTSTRAPS=10	
+declare -ri BOOTSTRAPS=1	
 declare -r R_DISTANCE_THRESHOLD=0.4
 
 declare -r MUSCLE_OUT=$INPUT_FILE.mfa
 declare -r DIST_NOREF=$INPUT_FILE.nrdist
-declare -r DIST_WREF=$INPUT_FILE.dist
 declare -r DIST_GNUPLOT=$INPUT_FILE.dist.plt
 declare -r DIST_IMAGE=$INPUT_FILE.dist.png
-declare -r NEIGHBORHOODS=$INPUT_FILE.nbhd
-declare -r NHBD_GNUPLOT=$INPUT_FILE.nbhd.plt
-declare -r NHBD_IMAGE=$INPUT_FILE.nbhd.png
 
 ################################################################
 # Main
@@ -223,10 +165,5 @@ echo "Extracting distances for ${labels[*]}"
 extract_distances_noref
 echo "Plotting classic bootscan"
 plot_classic
-
-
-echo "Generating Neighborhoods file"
-extract_distances
-extract_neighborhoods
-echo "Plotting"
-plot_neighborhood
+echo "Cleaning up"
+cleanup # comment this to see the temporary results
