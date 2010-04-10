bc7886719d0477e131969d143c435d0e5564004e
Thomas Junier <thomas.junier@unige.ch>
2010-04-10 20:13:08 +0000

    uses fabs() instead of cabs()

--- tests/test_tree_models.c
+++ tests/test_tree_models.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <complex.h> /* needed for cabs() */
 #include <stdlib.h>
+#include <math.h> 
 
 #include "rnode.h"
 #include "list.h"
@@ -14,31 +14,30 @@ int test_reciprocal_exponential_CDF()
 	const char *test_name = "test_reciprocal_exponential_CDF";
 
 	double val = reciprocal_exponential_CDF(0,1);
-	/* we use cabs() instead of abs() b/c it returns a double */
-	if (cabs(val - 0) > TOLERANCE) {
+	if (fabs(val - 0) > TOLERANCE) {
 		printf ("%s: expected 0, got %g\n", test_name, val);
 		return 1;
 	}
 	val = reciprocal_exponential_CDF(0,10);
-	if (cabs(val - 0) > TOLERANCE) {
+	if (fabs(val - 0) > TOLERANCE) {
 		printf ("%s: expected 0, got %g\n", test_name, val);
 		return 1;
 	}
 	val = reciprocal_exponential_CDF(0,.1);
-	if (cabs(val - 0) > TOLERANCE) {
+	if (fabs(val - 0) > TOLERANCE) {
 		printf ("%s: expected 0, got %g\n", test_name, val);
 		return 1;
 	}
 	val = reciprocal_exponential_CDF(0.5,1);
 	double exp = 0.693147; /* -ln(0.5) */
-	if (cabs(val - exp) > TOLERANCE) {
-		printf ("%s: expected %g, got %g (diff: %g)\n", test_name, exp, val, cabs(val-exp));
+	if (fabs(val - exp) > TOLERANCE) {
+		printf ("%s: expected %g, got %g (diff: %g)\n", test_name, exp, val, fabs(val-exp));
 		return 1;
 	}
 	val = reciprocal_exponential_CDF(0.5,3);
 	exp = 0.693147 / 3;
-	if (cabs(val - exp) > TOLERANCE) {
-		printf ("%s: expected %g, got %g (diff: %g)\n", test_name, exp, val, cabs(val-exp));
+	if (fabs(val - exp) > TOLERANCE) {
+		printf ("%s: expected %g, got %g (diff: %g)\n", test_name, exp, val, fabs(val-exp));
 		return 1;
 	}
 
@@ -59,7 +58,7 @@ int test_tlt_grow_node()
 	double grown_length = strtod(kid->edge_length_as_string, NULL);
 
 	double exp = prior_remaining_time - posterior_remaining_time;
-	if (cabs(grown_length - exp) > TOLERANCE) {
+	if (fabs(grown_length - exp) > TOLERANCE) {
 		printf ("%s: expected %g, got %g\n", test_name, exp,
 				grown_length);
 		return 1;
