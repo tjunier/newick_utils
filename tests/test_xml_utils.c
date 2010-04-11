18e36ce62330d9d09999cdb412871f3d11963dfc
Thomas Junier <thomas.junier@unige.ch>
2010-04-11 21:23:55 +0000

    fixed typos

--- tests/test_xml_utils.c
+++ tests/test_xml_utils.c
@@ -17,14 +17,14 @@ int test_escape_predefined_character_entities()
 	const char *s2 = "an & ampersand";
 	if (strcmp(escape_predefined_character_entities(s2), 
 				"an &amp; ampersand") != 0) {
-		printf ("%s: expected 'an &amp; ampersand', got '%s'\n", test_name, escape_predefined_character_entities(s1));
+		printf ("%s: expected 'an &amp; ampersand', got '%s'\n", test_name, escape_predefined_character_entities(s2));
 			return 1;
 	}
 
 	const char *s3 = "& < > ' \"";
 	if (strcmp(escape_predefined_character_entities(s3), 
 				"&amp; &lt; &gt; &apos; &quot;") != 0) {
-		printf ("%s: expected '&amp; &lt; &gt; &apos; &quot;', got '%s'\n", test_name, escape_predefined_character_entities(s1));
+		printf ("%s: expected '&amp; &lt; &gt; &apos; &quot;', got '%s'\n", test_name, escape_predefined_character_entities(s3));
 			return 1;
 	}
 
@@ -32,10 +32,9 @@ int test_escape_predefined_character_entities()
 	const char *exp = "&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;";
 	if (strcmp(escape_predefined_character_entities(s4), exp) != 0) {
 		printf ("%s: expected '%s', got '%s'\n", test_name, exp, 
-			escape_predefined_character_entities(s1));
+			escape_predefined_character_entities(s4));
 			return 1;
 	}
-
 	printf("%s ok.\n", test_name);
 	return 0;
 }
@@ -43,7 +42,7 @@ int test_escape_predefined_character_entities()
 int main()
 {
 	int failures = 0;
-	printf("Starting lca test...\n");
+	printf("Starting XML utilities test...\n");
 	failures += test_escape_predefined_character_entities();
 	if (0 == failures) {
 		printf("All tests ok.\n");
