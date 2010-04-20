b83643c5f1f64951160a2b2f47d57f5a589c79a9
Thomas Junier <thomas.junier@unige.ch>
2010-04-20 13:44:36 +0000

    nw_reroot.py now also demonstrates a "thin" interface (direct libnw calls)

--- src/nw_reroot.py
+++ src/nw_reroot.py
@@ -16,16 +16,23 @@ def main():
 
 	label = sys.argv[2]
 
+# "Thin" interface: mostly direct calls to libnw functions.
+
+	if filename != '':
+		libnw.set_parser_input_filename(filename)
+	tree = libnw.parse_tree()
+	lbl2node = libnw.create_label2node_map(tree.contents.nodes_in_order)
+	node = libnw.hash_get(lbl2node, label)
+	node = cast(node, POINTER(rnode))
+	libnw.reroot_tree(tree, node)
+	libnw.dump_newick(tree.contents.root)
+
+#   The following does the same thing, but with a more object-oriented
+#   interface. Use it if you need to manipulate the tree from Python.
 #	for tree in Tree.parse_newick_input(filename):
 #		outgroup = tree.get_node_with_label(label)
 #		tree.reroot(outgroup)
 #		tree.dump_newick()
-
-	tree = libnw.parse_tree()
-	lbl2node = libnw.create_label2node_map(tree.contents.nodes_in_order)
-	node = libnw.hash_get(lbl2node, "Homo")
-	node = cast(node, POINTER(rnode))
-	print node.contents.label
 
 if __name__ == "__main__":
 	main()
