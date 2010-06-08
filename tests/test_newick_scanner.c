dd96c88f1cd5056acb1c7924a1303565f987c7fe
Thomas Junier <thomas.junier@unige.ch>
2010-06-08 16:22:54 +0200

    + label test (fails)

--- tests/test_newick_scanner.c
+++ tests/test_newick_scanner.c
@@ -239,12 +239,56 @@ int test_catenated_quoted_labels()
 	return 0;
 }
 
+int test_label_chars()
+{
+	const char *test_name = "test label chars";
+
+	char *newick = "(la/bel)";
+	newick_scanner_set_string_input(newick);
+
+	int token_type = -1;
+
+	token_type = nwslex();
+	if (O_PAREN != token_type) {
+		printf ("%s: expected token type %d, got %d\n",
+				test_name, O_PAREN, token_type);
+		return 1;
+	}
+	token_type = nwslex();
+	if (LABEL != token_type) {
+		printf ("%s: expected token type %d, got %d\n",
+				test_name, LABEL, token_type);
+		return 1;
+	}
+	if (strcmp(nwslval.sval, "la/bel") != 0) {
+		printf ("%s: expected label 'la/bel', got '%s'\n",
+				test_name, nwslval.sval);
+		return 1;
+	}
+	token_type = nwslex();
+	if (C_PAREN != token_type) {
+		printf ("%s: expected token type %d, got %d\n",
+				test_name, C_PAREN, token_type);
+		return 1;
+	}
+	token_type = nwslex();	/* EOF */
+	if (token_type > 0) {
+		printf ("%s: expected EOF, got %d\n",
+				test_name, EOF, token_type);
+		return 1;
+	}
+
+	printf("%s ok.\n", test_name);
+	return 0;
+}
+
 int main()
 {
 	int failures = 0;
 	printf("Starting newick scanner test...\n");
 	failures += test_simple();
 	failures += test_garbled();
+	failures += test_label_chars();
 	failures += test_quoted_labels();
 	failures += test_catenated_quoted_labels();
 	if (0 == failures) {
