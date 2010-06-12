1a8f3634a58682f93ced685a9dd21b749afef3ef
Thomas Junier <thomas.junier@unige.ch>
2010-06-12 21:23:49 +0200

    All tests ok; comments for newick parser tests

--- tests/test_newick_parser.c
+++ tests/test_newick_parser.c
@@ -6,19 +6,58 @@
 #include "parser.h"
 #include "newick_parser.h"
 #include "tree.h"
+#include "to_newick.h"
 
 int nwslex (void);
 struct rnode *root;
 struct llist *nodes_in_order;
 enum parser_status_type newick_parser_status;
 
+/* NOTE: we can use to_newick() to check the parser's output because this
+ * function is independently tested on trees constructed without the parser
+ * (see test_to_newick) */
+
+int check_tree(char *test_name, char *newick)
+{
+	newick_scanner_set_string_input(newick);
+
+	struct rooted_tree *tree = parse_tree();
+	char *obt = to_newick(tree->root);
+	if (strcmp(obt, newick) != 0) {
+		printf ("%s: expected '%s', got '%s'\n", test_name,
+				newick, obt);
+		return 1;
+	}
+
+	return 0;
+}
+
+
 int test_simple()
 {
-	const char *test_name = "test_simple";
+	char *test_name = "test_simple";
 
-	/* Note: this is a valid Newick string, but since we're checking the
-	 * scanner, it doesn't have to be - see test_garbled()  */
 	char *newick = "((A,B),C);";
+
+	int result = check_tree(test_name, newick);
+
+	if (0 == result) {
+		printf ("%s: ok.\n", test_name);
+		return 0;
+	} else
+		return 1;
+}
+
+/* The test__jf_* tests all test trees from Felsenstein's description of Newick
+ * at http://evolution.genetics.washington.edu/phylip/newicktree.html, which is
+ * the closest thing to an official description. */
+
+int test_jf_1()
+{
+
+	char *test_name = "test_jf_1";
+
+	char *newick = "(B,(A,C,E),D);";
 
 	newick_scanner_set_string_input(newick);
 
@@ -39,6 +78,7 @@ int main()
 	int failures = 0;
 	printf("Starting newick parser test...\n");
 	failures += test_simple();
+	failures += test_jf_1();
 	if (0 == failures) {
 		printf("All tests ok.\n");
 	} else {
--- tests/test_summary.sh
+++ tests/test_summary.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 # NOTE: this script is not really a test, rather, it counts how many test
-# clauses of each kind (Unit or App) there are.  The TESTS variable in
+# cases of each kind (Unit or App) there are.  The TESTS variable in
 # Makefile.am is set so that this program gets run last, just before the
 # failure/pass count.
 
-nb_unit_test_clauses=$( grep 'failures +=' test_*.c | wc -l)
-nb_app_test_clauses=$(
+# What counts as a test case is: i) any test_* function in the unit tests, and
+# ii) any line in the app tests (see below). A unit test case generally tests
+# several assertions.
+
+nb_unit_test_cases=$( grep 'failures +=' test_*.c | wc -l)
+nb_app_test_cases=$(
 	wc -l test_nw_*_args | grep total | awk '{print $1}')
 
-printf "%0d unit test clauses\n" $nb_unit_test_clauses
-printf "%0d application test clauses\n" $nb_app_test_clauses
-printf "total: %0d clauses.\n" $((nb_app_test_clauses + 
-	nb_unit_test_clauses))
+printf "%0d unit test cases\n" $nb_unit_test_cases
+printf "%0d application test cases\n" $nb_app_test_cases
+printf "total: %0d cases.\n" $((nb_app_test_cases + 
+	nb_unit_test_cases))
