e8d6655cc19663d7232952607af1bda493c1a42f
Thomas Junier <thomas.junier@unige.ch>
2010-09-20 12:18:32 +0200

    basic XPath working

--- src/xml_test.c
+++ src/xml_test.c
@@ -2,13 +2,15 @@
 #include <string.h>
 #include <libxml/tree.h>
 #include <libxml/parser.h>
+#include <libxml/xpath.h>
 
 int main(int argc, char *argv[])
 {
 	const char * dummy_doc_start = "<dummy>";
 	const char * dummy_doc_stop = "</dummy>";
 
-	char *svg_snippet = argv[1];
+	xmlChar *xpath = (xmlChar *) argv[1];
+	char *svg_snippet = argv[2];
 	xmlDocPtr doc;
 	size_t snippet_length = strlen(svg_snippet);
 	int doc_length;
@@ -28,7 +30,7 @@ int main(int argc, char *argv[])
 	strcpy(doc_str, dummy_doc_start);
 	strcpy(doc_str + start_length, svg_snippet);
 	strcpy(doc_str + start_length + snippet_length, dummy_doc_stop);
-	
+
 	printf ("dummy doc: %s\n", doc_str);
 
 	/* parse SVG from string */
@@ -38,7 +40,29 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	free(doc_str);
+
+	/* look for x* attributes in elements */
+	xmlXPathContextPtr context = xmlXPathNewContext(doc);
+	xmlXPathObjectPtr result = xmlXPathEvalExpression(xpath, context);
+	if (xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+		xmlXPathFreeObject(result);
+		printf ("empty.\n");
+		return EXIT_SUCCESS;
+	} else {
+		xmlNodeSetPtr nodeset = result->nodesetval;
+		int i;
+		xmlChar *keyword;
+		for (i=0; i < nodeset->nodeNr; i++) {
+			keyword = xmlNodeListGetString(doc,
+				nodeset->nodeTab[i]->xmlChildrenNode, 1);
+			printf("keyword: %s\n", keyword);
+			xmlFree(keyword);
+		}
+		xmlXPathFreeObject (result);
+	}
+
 	xmlFreeDoc(doc);
+	xmlXPathFreeContext(context);
 
 	return EXIT_SUCCESS;
 }
