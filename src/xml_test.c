89aef932c89b212942a69f4413f8bc8f8f690c19
Thomas Junier <thomas.junier@unige.ch>
2010-09-21 07:18:48 +0200

    refectored wrapping and altering -> own f()

--- src/xml_test.c
+++ src/xml_test.c
@@ -6,70 +6,78 @@
 
 #include "masprintf.h"
 
-int main(int argc, char *argv[])
+/* Wraps an SVG snipped in a dummy document (have to do this for the snippet to
+ * parse as an XML doc, unless there happens to be just one element). */
+
+static char * wrap_in_dummy_doc(char *svg_snippet)
 {
 	const char * dummy_doc_start = "<dummy>";
 	const char * dummy_doc_stop = "</dummy>";
-
-	xmlChar *xpath = (xmlChar *) argv[1];
-	char *svg_snippet = argv[2];
-	xmlDocPtr doc;
-	size_t snippet_length = strlen(svg_snippet);
-	int doc_length;
 	char *doc_str;
 
-	//printf ("snippet: %s\n", svg_snippet);
 
 	/* wrap snippet into dummy document */
-	int start_length = strlen(dummy_doc_start);
-	int stop_length = strlen(dummy_doc_stop);
-	doc_length = start_length + snippet_length + stop_length;
+	size_t start_length = strlen(dummy_doc_start);
+	size_t snippet_length = strlen(svg_snippet);
+	size_t stop_length = strlen(dummy_doc_stop);
+	size_t doc_length = start_length + snippet_length + stop_length;
 	doc_str = malloc(doc_length * sizeof(char));
-	if (NULL == doc_str) {
-		perror(NULL);
-		return EXIT_FAILURE;
-	}
+	if (NULL == doc_str) return NULL;
+
 	strcpy(doc_str, dummy_doc_start);
 	strcpy(doc_str + start_length, svg_snippet);
 	strcpy(doc_str + start_length + snippet_length, dummy_doc_stop);
 
-	// printf ("dummy doc: %s\n", doc_str);
+	return doc_str;
+}
 
-	/* parse SVG from string */
-	doc = xmlParseMemory(doc_str, doc_length);
-	if (NULL == doc) {
-		fprintf(stderr, "Failed to parse document\n");
-		return EXIT_FAILURE;
-	}
-	free(doc_str);
-
-	/* look for x* attributes in elements */
+static void alter_x_attr(xmlDocPtr doc, char *attr)
+{
+	/* look for xpath attributes in elements */
+	xmlChar *xpath = (xmlChar *) masprintf("//*[@%s]", attr);
 	xmlXPathContextPtr context = xmlXPathNewContext(doc);
 	xmlXPathObjectPtr result = xmlXPathEvalExpression(xpath, context);
 	if (xmlXPathNodeSetIsEmpty(result->nodesetval)) {
 		xmlXPathFreeObject(result);
-		printf ("empty.\n");
-		return EXIT_SUCCESS;
+		return;
 	} else {
 		xmlNodeSetPtr nodeset = result->nodesetval;
 		int i;
 		for (i=0; i < nodeset->nodeNr; i++) {
 			xmlNodePtr node = nodeset->nodeTab[i];
-			// printf("node name: %s\n", (char *) node->name);
-			// printf("node type: %d\n", node->type);
-			xmlChar *value = xmlGetProp(node, (xmlChar *) "x");
+			xmlChar *value = xmlGetProp(node, (xmlChar *) attr);
 			if (NULL != value) {
-				// printf("x-value: %s\n", (char *) value);
 				double x_val = atof((char *) value);
 				x_val += 1.0;
 				char *new_value = masprintf("%g", x_val);
-				xmlSetProp(node, (xmlChar *) "x", (xmlChar *) new_value);
+				xmlSetProp(node, (xmlChar *) attr,
+						(xmlChar *) new_value);
 			}	
 			xmlFree(value);
 		}
 		xmlXPathFreeObject (result);
 	}
-	
+	xmlFree(xpath);
+	xmlXPathFreeContext(context);
+}	
+
+int main(int argc, char *argv[])
+{
+	char *svg_snippet = argv[1];
+	xmlDocPtr doc;
+
+	char *wrapped_snippet = wrap_in_dummy_doc(svg_snippet);
+	if (NULL == wrapped_snippet) { perror(NULL); exit(EXIT_FAILURE); }
+
+	/* parse SVG from string */
+	doc = xmlParseMemory(wrapped_snippet, strlen(wrapped_snippet));
+	if (NULL == doc) {
+		fprintf(stderr, "Failed to parse document\n");
+		return EXIT_FAILURE;
+	}
+	free(wrapped_snippet);
+
+	alter_x_attr(doc, "x");
 	/* now print out each node in the <dummy> doc, whether changed or not
 */
 	
@@ -100,7 +108,6 @@ int main(int argc, char *argv[])
 
 	free(tweaked_svg);
 	xmlFreeDoc(doc);
-	xmlXPathFreeContext(context);
 
 	return EXIT_SUCCESS;
 }
