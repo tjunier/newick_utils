030f9218a5fdebf81525a8a0317db7e4a30b3bcc
Thomas Junier <thomas.junier@unige.ch>
2010-09-27 08:39:35 +0200

    declaration no longer needed

--- src/xml_utils.h
+++ src/xml_utils.h
@@ -34,9 +34,3 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * or NULL is returned if no space could be allocated.  * */
 
 char *escape_predefined_character_entities(const char*);
-
-/* Changes the sign of x-attributes of SVG elements. This is used to correct
- * the effects of rotations and tranformations on the left side of radial
- * graphs. */
-
-char *change_svg_x_attr_sign(const char *svg_snippet);
