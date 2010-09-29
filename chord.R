226b2df3b70a4fda067e8c2921e052655f3270ed
Thomas Junier <thomas.junier@unige.ch>
2010-09-29 17:43:12 +0200

    chord.R: graph now ok

--- chord.R
+++ chord.R
@@ -10,19 +10,37 @@
 # I can round off angles up to theta.
 
 # Therefore, I will look at various values of theta and look at the chord
-# function assuming 1 pixel. The length of the chord is given by:
+# function assuming 1 pixel. The length c of the chord is given by:
 #
-# r crd(theta)
+# c = r crd(theta)
 #
 # where crd(theta) = 2 sin(theta/2)
 #
-# so, if we fix r crd(theta) == 1 (pixel), we find
+# so, for a given resolution c, we have
 #
-# r = 1 / (2 sin(theta/2))
+# r = c / crd(theta) = c / 2 sin(theta/2)
 
-radius <- function(theta)	# assuming chord length is 1 px
+radius <- function(theta, c)	# assuming chord length is 1 px
 {
-	return(1/(2*sin(theta/2)))
+	return(c/(2*sin(theta/2)))
 }
 
-# try some angles (SVG works in degrees, so I'll use degrees)
+# try some small angles (SVG works in degrees, so I'll use degrees). Each angle
+# is 10 times wider than the previous one.
+
+angles_deg <- 10 ** seq(-2.5,0,0.2)
+angles_rad <- angles_deg / 360 * 2*pi
+rad_2 <- radius(angles_rad, 2)
+rad_1 <- radius(angles_rad, 1)
+rad_0.5 <- radius(angles_rad, 0.5)
+
+line.width <- 2
+plot(angles_deg, rad_1, type='l', lwd=line.width, log='x',
+	main='Radius as a f() of angle\nat various resolutions (chord lengths)',
+	xlab='angle [°]', ylab='radius')
+lines(angles_deg, rad_0.5, lwd=line.width, col='blue')
+lines(angles_deg, rad_2, lwd=line.width, col='red')
+
+legend("topright", legend=c(0.5, 1, 2), col=c('blue', 'black', 'red'),
+	title='chord', lwd=line.width, inset=c(0.1,0.1))
+
