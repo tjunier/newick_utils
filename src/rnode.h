5ec924fcc812c46717dda2d51cbbccbd19d2fa3b
Thomas Junier <thomas.junier@unige.ch>
2011-01-07 15:34:06 +0100

    Redefined struct rnode. No more children list; uses pointers to next sibling and 1st child

--- src/rnode.h
+++ src/rnode.h
@@ -27,51 +27,28 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-/* 
 
-Copyright (c) 2009 Thomas Junier and Evgeny Zdobnov, University of Geneva
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-* Neither the name of the University of Geneva nor the names of its
-    contributors may be used to endorse or promote products derived from this
-    software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-struct llist;
 struct rnode;
 
 struct rnode {
-	struct rnode *parent;
+
 	/* When we speak of 'the' edge of a node, we mean the edge that leads
-	 * to its parent. */
-	char *edge_length_as_string;	/* Most of the time this is enough, but... */
-	double edge_length; 		/* ...use this when numerical value is needed */
-	struct llist *children;
+	 * to its parent. Its value is numeric, and it can be zero or even
+	 * negative (NJ trees), it can also be undefined (cladograms).
+	 * Therefore we store it as a string, with "" indicating an undefined
+	 * length. This is how Newick does it anyway. */
+	char *edge_length_as_string;	
+	double edge_length; /* TODO: check if we use this. */
 	char *label;
 	void *data;	/* app-dependent data for this node */
 
+	struct rnode *parent;		/* NULL iff root */
+	struct rnode *next_sibling;
+	int child_count;		/* 0 iff leaf */
+	struct rnode *first_chlid;	/* NULL iff leaf */
+
 	/* enables traversing by rnode_iterator_next() */
-	struct list_elem *current_child_elem;
+	struct rnode *current_child;
 	int seen;	// TODO: check if we could use a boolean.
 };
 
