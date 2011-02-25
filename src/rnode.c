0aa38d36fa0ba3c3117a5ccc620cdc188b314e9c
Thomas Junier <thomas.junier@unige.ch>
2011-02-25 17:13:01 +0100

    fixed segfaulting bug

--- src/rnode.c
+++ src/rnode.c
@@ -198,21 +198,16 @@ void free_descendants(struct rnode *node)
 
 struct rnode** children_array(struct rnode *node)
 {
-	printf("kid[] for %p (%s)\n", node, node->label);
 	struct rnode **array = malloc(node->child_count *
 			sizeof(struct rnode *));
 	if (NULL == array) return NULL;
 	memset(array, '\0', node->child_count);
 
 	struct rnode *kid = NULL;
-	int i = -1;
-	for (kid = node->first_child, i = 0;
-		NULL != kid, i < node->child_count;
-			kid = kid->next_sibling, i++) {
-		printf("\ti: %d(%d), kid: %p (%s), next sib: %p\n",
-				i, node->child_count, kid, kid->label,
-				kid->next_sibling);
+	int i = 0;
+	for (kid = node->first_child; NULL != kid; kid = kid->next_sibling) {
 		array[i] = kid;
+		i++;
 	}
 
 	return array;
