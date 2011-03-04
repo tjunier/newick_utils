a7429a2299ecc5fcfeaad2815553b8a13dc188f7
Thomas Junier <thomas.junier@unige.ch>
2011-03-04 10:24:34 +0100

    test partition f() ok

--- src/test-eval-list.scm
+++ src/test-eval-list.scm
@@ -1,4 +1,5 @@
 (use-modules (ice-9 rdelim))
+(use-modules (srfi srfi-1))
 
 (lambda (lst)    ; L: (test1 test2 ... testn)
 	(letrec ((eval-tests (lambda (L)
@@ -13,17 +14,34 @@
 	(eval-tests lst)))
 
 
-(define categorize-tests
- 	(lambda (lst)
-	 (letrec ((categ-tests
-						 (lambda (L)
-							(if (null? L) '()
-							 (let* ((test (car L))
-											(clause (car test)))
-								(case (clause)
-								 (('begin) (format #t "begin ~a~%" test))
-								 (else		 (format #t "main ~a~%" test))))))))
-		(categ-tests lst))))
+(define partition-test-list
+	(lambda (lst)
+		(let ((begin-test-list 				'())
+					(begin-tree-test-list 	'())
+					(within-tree-test-list	'())
+					(end-test-list					'())
+					(end-tree-test-list			'()))
+
+			(for-each (lambda (test)
+									(cond
+										((eq? (car test) 'begin)
+										 (set! begin-test-list (cons test begin-test-list)))
+										((eq? (car test) 'begin-tree)
+										 (set! begin-tree-test-list (cons test begin-tree-test-list)))
+										((eq? (car test) 'end)
+										 (set! end-test-list (cons test end-test-list)))
+										((eq? (car test) 'end-tree)
+										 (set! end-tree-test-list (cons test end-tree-test-list)))
+										(else
+										 (set! within-tree-test-list (cons test within-tree-test-list)))))
+
+								lst)
+
+			(list (reverse begin-test-list)
+						(reverse begin-tree-test-list)
+						(reverse within-tree-test-list)
+						(reverse end-tree-test-list)
+						(reverse end-tree-test-list)))))
 
 ;; Evaluates a list of tests. A test is a (clause action) pair. Iff the clause
 ;; is #t, the action gets evaluated. The function returns #t iff at least one
@@ -65,3 +83,14 @@
 						(#f	'())
 						(#f	'())
 						))
+
+(define l4 '(
+						 (begin (define num-trees 0))
+						 (begin-tree (begin (define num-nodes 0)
+																(define num-leaves 0)
+																(set! num-trees (+ 1 num-trees))))
+						 (#t (set! num-nodes (+ 1 num-nodes)))
+						 (l  (set! num-leaves (+ 1 num-leaves)))
+						 (end-tree (format "# nodes: ~d~%" num-nodes))
+						 (end (format #t "# trees: ~d~%" num-trees))
+						 ))
