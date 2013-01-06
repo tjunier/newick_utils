87a537d9bae6aff45140059f9d20f0e9276c6da2
Thomas Junier <thomas.junier@epfl.ch>
2013-01-06 22:47:01 +0100

    porting to 5.2

--- src/lua_tree_editor.c
+++ src/lua_tree_editor.c
@@ -613,6 +613,33 @@ static void set_predefined_variables(struct rnode *node, lua_State *L)
 	assert(lua_gettop(L) == initial_lua_stack_size);
 }
 
+/* Pushes onto the stack the function at index '*phase' in the globals table. */
+// TODO: try lua_getglobal!!!
+
+static SEE_TODO_ABOVE void get_user_hook(lua_State *L, const char *phase)
+{
+		const lua_Number *version = lua_version(L);
+		lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS);
+		lua_getfield(L, -1, NODE);
+		//lua_getfield(L, LUA_GLOBALSINDEX, NODE);
+}
+
+/* Sets the element at index '*phase' in the globals table to the hook function
+ * at the top of the stack. */
+
+static void set_user_hook(lua_State *L, const char *phase)
+{
+		/* hook is at -1 */
+		const lua_Number *version = lua_version(L);
+		lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS);
+		/* hook at -2, globals table at -1 */
+		lua_insert(L, -2);
+		/* globals at -2, hook at -1 */
+		lua_setfield(L, -2, NODE);
+		//lua_getfield(L, LUA_GLOBALSINDEX, NODE);
+}
+
+
 static void process_tree(struct rooted_tree *tree, lua_State *L,
 		struct parameters params)
 {
@@ -649,7 +676,7 @@ static void process_tree(struct rooted_tree *tree, lua_State *L,
 		} 
 
 		set_predefined_variables(current_node, L);
-		lua_getfield(L, LUA_GLOBALSINDEX, NODE);
+		get_user_hook(L, NODE);
 		lua_call(L, 0, 0);
 	} /* loop over all nodes */
 
@@ -756,7 +783,7 @@ static void load_lua_condition(lua_State * L, char *lua_condition)
 		printf("%s\n", msg);
 		exit(EXIT_FAILURE);
 	}
-	lua_setfield(L, LUA_GLOBALSINDEX, CONDITION);
+	set_user_hook(L, CONDITION);
 }
 
 static void load_lua_action(lua_State *L, char *lua_action)
@@ -768,7 +795,7 @@ static void load_lua_action(lua_State *L, char *lua_action)
 		printf("%s\n", msg);
 		exit(EXIT_FAILURE);
 	}
-	lua_setfield(L, LUA_GLOBALSINDEX, ACTION);
+	set_user_hook(L, ACTION);
 }
 
 static void run_user_hooks_file(lua_State * L, char *user_hooks_filename)
@@ -927,7 +954,7 @@ static int lua_cli_process_node(lua_State *L)
 	if (NULL == lnode)
 		luaL_error(L, "N is not a node");
 
-	lua_getfield(L, LUA_GLOBALSINDEX, CONDITION);
+	get_user_hook(L, CONDITION);
 	lua_call(L, 0, 1);
 	if (lua_isboolean(L, -1) != 1) {
 		fprintf(stderr, "WARNING: condition does not evaluate "
@@ -938,7 +965,7 @@ static int lua_cli_process_node(lua_State *L)
 	int match = lua_toboolean(L, -1);
 	lua_pop(L, 1);
 	if (match) {
-		lua_getfield(L, LUA_GLOBALSINDEX, ACTION);
+		get_user_hook(L, ACTION);
 		lua_call(L, 0, 0);
 		/* see -o switch */
 		lua_getfield(L, LUA_GLOBALSINDEX, STOP_AT_1ST_MATCH);
@@ -979,6 +1006,7 @@ static int luaopen_lnode (lua_State *L)
 	return 0;
 }
 
+
 static void run_user_hook(lua_State *L, const char *phase)
 {
 	lua_getfield(L, LUA_GLOBALSINDEX, phase);
@@ -1017,6 +1045,7 @@ int main(int argc, char* argv[])
 	}
 
 	if (NULL != params.user_hooks_filename) {
+		/* Get the node() function defined by the user */
 		run_user_hooks_file(L, params.user_hooks_filename);
 		lua_getfield(L, LUA_GLOBALSINDEX, NODE);
 		int NODE_type = lua_type(L, -1);
