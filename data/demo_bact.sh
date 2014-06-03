26e3f8eee6aaa7a111478577f1cb6f0bc17cfd74
Thomas Junier <thomas.junier@epfl.ch>
2014-06-03 06:07:23 +0200

    data for CUSO examples

---
+++ data/demo_bact.sh
@@ -0,0 +1,6 @@
+nw_reroot bacterial_orthologs Nitrosopumilus \
+| nw_topology - \
+| nw_order - \
+| sort \
+| uniq -c \
+| sort -n		
---
+++ data/demo_hiv.sh
@@ -0,0 +1,3 @@
+nw_topology -I hiv.nw \
+| nw_display -sr -w 1200 -l 'font-size:0' \
+	-W 0 -c patient_css.map -
